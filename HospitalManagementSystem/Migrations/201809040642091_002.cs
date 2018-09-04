0e10c887dddc52ca5012ced9a633f172354b04aa
ProbinAdhikari <proraj9@gmail.com>
2018-09-04 16:43:04 +1000

    Enablr Migration and commit 002 database version to solve migration issue

---
+++ HospitalManagementSystem/Migrations/201809040642091_002.cs
@@ -0,0 +1,16 @@
+namespace HospitalManagementSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _002 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
